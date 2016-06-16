c221de5b4a52a9b2681fd21d73af394b99c3f316
Nemanja Stojanovic <n.stojanovic035@gmail.com>
2016-06-16 13:11:05 -0400

    global fill color

--- pages/loader.js
+++ pages/loader.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-  window.fillColor = '#c51162';
+  window.fillColor = '#00e676';
   window.d3.json('https://www.reddit.com/r/pics.json', (error, json) => {
     if (error) {
       return console.error(error);
