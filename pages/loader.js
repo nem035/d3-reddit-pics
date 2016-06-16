014e9c5edac74d1f9a79a0eb2ae34adbb8633d34
Nemanja Stojanovic <n.stojanovic035@gmail.com>
2016-06-16 13:05:10 -0400

    added live data loading

--- pages/loader.js
+++ pages/loader.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', (event) => {
   window.fillColor = '#c51162';
-  window.d3.json('../../pics.json', (error, json) => {
+  window.d3.json('https://www.reddit.com/r/pics.json', (error, json) => {
     if (error) {
       return console.error(error);
     }
